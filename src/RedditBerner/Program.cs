0f7acbab8c0bae9a5c2632bfe9567641a086dcbe
Kris Craig <KrisCraig@php.net>
2019-09-07 01:36:19 -0700

    Created VS solution.

---
+++ src/RedditBerner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RedditBerner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
